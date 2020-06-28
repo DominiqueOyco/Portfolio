b72781538453ba1e88c0db3051515bd09d2f59e4
DominiqueOyco <dominickoyco@gmail.com>
2020-06-28 15:34:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function directToLinkedIn() {
-  location.replace("https://www.linkedin.com/in/dominique-oyco-27267abb/", "_blank")
+  window.open("https://www.linkedin.com/in/dominique-oyco-27267abb/", "_blank")
 }
 
 function directToGithub() {
-  location.replace("https://github.com/DominiqueOyco?tab=repositories", "_blank")
+  window.open("https://github.com/DominiqueOyco?tab=repositories", "_blank")
 }
