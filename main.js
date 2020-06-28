ac0bd91345201430ce3e844cfa71b77858ce5f4c
DominiqueOyco <dominickoyco@gmail.com>
2020-06-28 13:26:25 -0700

    updated javascript files

---
+++ main.js
@@ -0,0 +1,7 @@
+function directToLinkedIn() {
+  location.replace("https://www.linkedin.com/in/dominique-oyco-27267abb/")
+}
+
+function directToGithub() {
+  location.replace("https://github.com/DominiqueOyco?tab=repositories")
+}
